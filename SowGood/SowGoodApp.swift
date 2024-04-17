0fc8ff71eaf20d5a2aa42c4efc4cb234767e0467
thai-ru <robinthairu34@gmail.com>
2024-04-17 18:22:29 +0300

    Initial Commit

---
+++ SowGood/SowGoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SowGoodApp.swift
+//  SowGood
+//
+//  Created by Robinson Thairu on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SowGoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
